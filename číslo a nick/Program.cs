8fb1b826286f73274e7a8d0819d2f84a1bd016f2
jan.gergel <jan.gergel@VT1-02>
2022-03-07 08:45:05 +0100

    Přidejte soubory projektu.

---
+++ číslo a nick/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Dictionary<string, string> slovník = new Dictionary<string, string>();
+
+for (int i=0;i<4; i++)
+{
+    Console.WriteLine("Zadej jméno uživatele");
+    string Jmeno = Console.ReadLine();
+    Console.WriteLine("Zadej cislo uživatele");
+    string Cislo = Console.ReadLine();
+    slovník.Add(Jmeno, Cislo);
+}
+foreach (var radekSlovniku in slovník)
+{
+    Console.WriteLine($"Jméno:{radekSlovniku.Key}, Tel.Číslo: {radekSlovniku.Value}");
+
+}
